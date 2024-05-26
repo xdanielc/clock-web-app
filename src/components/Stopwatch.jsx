dd063c1cdef785e6b8d4dda17487bafc30f95599
xdanielc <xdanielcalderon@gmail.com>
2024-05-26 20:03:20 +0200

    Initial Stopwatch implementation

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
 
 const Stopwatch = () => {
+    const [isActive, setIsActive] = useState(false)
+    const [seconds, setSeconds] = useState(5) // Vales dependent on this change onMount
+    const [percentage, setPercentage] = useState(0)
+    const [totalSeconds, setTotalSeconds] = useState(0)
+    const minutesDisplay = Math.floor(seconds / 60);
+    const secondsDisplay = String(seconds % 60).padStart(2, '0')
+
+    useEffect(() => {
+        setTotalSeconds(seconds)
+        setPercentage(seconds)
+    }, [])
+
+    useEffect(() => {
+        let intervalId;
+        if (isActive && seconds > 0) {
+            if(seconds === totalSeconds){
+                setPercentage(totalSeconds-1)
+            }
+            intervalId = setInterval(() => {
+                // Functional update gets the state
+                setSeconds(prevSeconds => {
+                    const newSeconds = prevSeconds - 1;
+                    setPercentage(newSeconds-1);
+                    return newSeconds;
+                });
+            }, 1000);
+        } else if (seconds === 0) {
+            clearInterval(intervalId);
+            setIsActive(false)
+        }
+        return () => clearInterval(intervalId);
+    }, [isActive, seconds]);
+
+    const beginTimeout = () => {
+        setIsActive(true);
+    };
+
     return (
         <>
-            {/* <div className='placeholder'>Stopwatch</div> */}
-            <svg viewBox="0 0 200 200" preserveAspectRatio="xMidYMid meet" className='responsiveCircle'>
-                {/* <circle cx="100" cy="100" r="70" strokeWidth={'2px'}/> */}
-                <path className="meter-arc" d="M 100,100 m -80,0 a 80,80 0 1,1 160,0 a 80,80 0 1,1 -160,0" />
-            </svg>
-            <button className='button-primary bottom'>INICIAR</button>
+            <div className="circular-container">
+                <CircularProgressbar
+                value={percentage}
+                maxValue={totalSeconds}
+                text={`${minutesDisplay}:${secondsDisplay}`}
+                strokeWidth={3}
+                styles={buildStyles({
+                    pathTransitionDuration: 1,
+                    pathTransition: 'linear',
+                    textColor: isActive ? 'var(--main-color)' : '#bbbbbb',
+                    pathColor: 'var(--main-color)'
+                })}
+                />
+            </div>
+            <div className="buttons-bottom">
+                <button className='button' onClick={beginTimeout}>Iniciar</button>
+            </div>
         </>
     )
 }
