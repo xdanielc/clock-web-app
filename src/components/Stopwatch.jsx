55547f49cce44c3f5002c4d2293956151256fa1a
xdanielc <xdanielcalderon@gmail.com>
2024-05-27 15:24:57 +0200

    Moved from stopwatch to timer, I got the names switched

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,64 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
-import 'react-circular-progressbar/dist/styles.css'
+import React from 'react'
 
 const Stopwatch = () => {
-    const [isActive, setIsActive] = useState(false)
-    const [seconds, setSeconds] = useState(5) // Vales dependent on this change onMount
-    const [percentage, setPercentage] = useState(0)
-    const [totalSeconds, setTotalSeconds] = useState(0)
-    const minutesDisplay = Math.floor(seconds / 60);
-    const secondsDisplay = String(seconds % 60).padStart(2, '0')
+    const timer = new Date()
 
-    useEffect(() => {
-        setTotalSeconds(seconds)
-        setPercentage(seconds)
-    }, [])
-
-    useEffect(() => {
-        let intervalId;
-        if (isActive && seconds > 0) {
-            if(seconds === totalSeconds){
-                setPercentage(totalSeconds-1)
-            }
-            intervalId = setInterval(() => {
-                // Functional update gets the state
-                setSeconds(prevSeconds => {
-                    const newSeconds = prevSeconds - 1;
-                    setPercentage(newSeconds-1);
-                    return newSeconds;
-                });
-            }, 1000);
-        } else if (seconds === 0) {
-            clearInterval(intervalId);
-            setIsActive(false)
-        }
-        return () => clearInterval(intervalId);
-    }, [isActive, seconds]);
-
-    const beginTimeout = () => {
-        setIsActive(true);
-    };
+    const hoursDisplay = String(timer.getHours()).padStart(2, '0')
+    const minsDisplay = String(timer.getMinutes()).padStart(2, '0')
+    const secsDisplay = String(timer.getSeconds()).padStart(2, '0')
 
     return (
         <>
-            <div className="circular-container">
-                <CircularProgressbar
-                value={percentage}
-                maxValue={totalSeconds}
-                text={`${minutesDisplay}:${secondsDisplay}`}
-                strokeWidth={3}
-                styles={buildStyles({
-                    pathTransitionDuration: 1,
-                    pathTransition: 'linear',
-                    textColor: isActive ? 'var(--main-color)' : '#bbbbbb',
-                    pathColor: 'var(--main-color)'
-                })}
-                />
-            </div>
-            <div className="buttons-bottom">
-                <button className='button' onClick={beginTimeout}>Iniciar</button>
-            </div>
+            <h2 className='crono-display'>{hoursDisplay}:{minsDisplay}:{secsDisplay}</h2>
         </>
     )
 }
