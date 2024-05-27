ec188a7b0f3174b09ba84953a26a778e265d1c54
xdanielc <xdanielcalderon@gmail.com>
2024-05-27 17:16:13 +0200

    Stopwatch implementation

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react';
 
-const Stopwatch = () => {
-    const timer = new Date()
+export const Stopwatch = () => {
 
-    const hoursDisplay = String(timer.getHours()).padStart(2, '0')
-    const minsDisplay = String(timer.getMinutes()).padStart(2, '0')
-    const secsDisplay = String(timer.getSeconds()).padStart(2, '0')
+    const intervalRef = useRef(null);
+    const [isActive, setIsActive] = useState(false);
+    const [ms, setMS] = useState(0);
+
+    const HourDisplay = String(parseInt(Math.floor(ms / 10) / 3600)).padStart(2, '0');
+    const MinDisplay = String(parseInt(Math.floor(ms / 10) / 60)).padStart(2, '0');
+    const SecDisplay = String(parseInt(Math.floor(ms / 10) % 60)).padStart(2, '0');
+    const msDisplay = ms % 10;
+
+    useEffect(() => {
+        if (isActive) {
+            intervalRef.current = setInterval(() => { setMS(prev => prev + 1); }, 100);
+        } else if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+        return () => {
+            if (intervalRef.current)
+                clearInterval(intervalRef.current);
+        };
+    }, [isActive]);
+
+    const toggle = () => {
+        setIsActive(prev => !prev);
+    };
 
     return (
         <>
-            <h2 className='crono-display'>{hoursDisplay}:{minsDisplay}:{secsDisplay}</h2>
+            <h2 className='crono-display'>{HourDisplay}:{MinDisplay}:{SecDisplay}.<span className='crono-ms'>{msDisplay}</span></h2>
+            <div className="buttons-bottom">
+                <button className={`button ${isActive ? '' : 'is-blue'}`} onMouseDown={toggle}>
+                    {isActive ? 'Stop' : 'Start'}
+                </button>
+            </div>
         </>
-    )
-}
-
-export default Stopwatch
+    );
+};
